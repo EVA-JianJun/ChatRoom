81aa5bcd9bb6fbe3e06fbba6535345afea9553d4
EVA-JianJun <eva_jianjun@163.com>
2022-07-28 14:43:06 +0800

    1.完善日志配置功能 2.邮件暂停只暂停90s

--- ChatRoom/gui.py
+++ ChatRoom/gui.py
@@ -69,6 +69,8 @@ class MyConfig():
                     "tag" : "Crimson",
                     # 超时(在这个时间前不发送邮件)
                     "deadline" : '1970-01-01 00:00:00',
+                    # 备注
+                    "note" : "ERR类型",
                 },
                 "INFO" : {
                     # 是否发送邮件
@@ -77,6 +79,8 @@ class MyConfig():
                     "tag" : None,
                     # 超时(在这个时间前不发送邮件)
                     "deadline" : '1970-01-01 00:00:00',
+                    # 备注
+                    "note" : "INFO类型",
                 },
                 # 默认配置
                 "DEFAULT" : {
@@ -86,6 +90,8 @@ class MyConfig():
                     "tag" : None,
                     # 超时(在这个时间前不发送邮件)
                     "deadline" : '1971-01-01 00:00:00',
+                    # 备注
+                    "note" : "DEFAULT类型",
                 }
             })
 
@@ -193,7 +199,7 @@ class RoomLog(ttk.Frame):
         def config_err():
             config_app = ttk.Toplevel(title="Config")
             config_app.iconbitmap(CONFIG_ICO_PATH)
-            config_app.geometry('900x500')
+            config_app.geometry('1000x650')
 
             # 上方配置栏
             config_frame = ttk.Frame(config_app, bootstyle=SECONDARY)
@@ -204,7 +210,7 @@ class RoomLog(ttk.Frame):
             button_frame.pack(fill=BOTH, side=TOP)
 
             # 插入一行新配置
-            def insert_a_line(all_config_controls_dict, name, mail, tag, deadline):
+            def insert_a_line(all_config_controls_dict, name, mail, tag, deadline, note):
                 # 一行配置
                 config_line_frame = ttk.Frame(config_frame)
                 config_line_frame.pack(fill=BOTH, side=TOP, ipadx=2, ipady=2)
@@ -214,16 +220,35 @@ class RoomLog(ttk.Frame):
                 entry1 = ttk.Entry(config_line_frame, width=7, bootstyle=PRIMARY)
                 entry1.insert(END, name)
                 entry1.pack(fill=BOTH, side=LEFT)
+
+                label5 = ttk.Label(config_line_frame, text=" 备注: ")
+                label5.pack(fill=BOTH, side=LEFT)
+                entry5 = ttk.Entry(config_line_frame, width=10, bootstyle=PRIMARY)
+                entry5.insert(END, note)
+                entry5.pack(fill=BOTH, side=LEFT)
+
                 label2 = ttk.Label(config_line_frame, text=" 邮件通知: ")
                 label2.pack(fill=BOTH, side=LEFT)
                 entry2 = ttk.Combobox(config_line_frame, width=2, value=("是", "否"), bootstyle=PRIMARY)
                 entry2.insert(END, mail)
                 entry2.pack(fill=BOTH, side=LEFT)
+
                 label3 = ttk.Label(config_line_frame, text=" 颜色标签: ")
+                if tag != "None":
+                    label3.config(background=tag)
                 label3.pack(fill=BOTH, side=LEFT)
                 entry3 = ttk.Combobox(config_line_frame, width=12, value=self.my_tag_list, bootstyle=PRIMARY)
+                def choose(event):
+                    # 选中事件
+                    tag = entry3.get()
+                    if tag == "None":
+                        label3.config(background="")
+                    else:
+                        label3.config(background=tag)
+                entry3.bind("<<ComboboxSelected>>", choose)
                 entry3.insert(END, tag)
                 entry3.pack(fill=BOTH, side=LEFT)
+
                 label4 = ttk.Label(config_line_frame, text=" 邮件禁用超时时间: ")
                 label4.pack(fill=BOTH, side=LEFT)
                 entry4 = ttk.Entry(config_line_frame, width=17, bootstyle=PRIMARY)
@@ -275,6 +300,8 @@ class RoomLog(ttk.Frame):
                         "tag"       : entry3,
                         # Entry
                         "deadline"  : entry4,
+                        # Entry
+                        "note"      : entry5,
                     }
 
             all_config_controls_dict = {}
@@ -288,6 +315,7 @@ class RoomLog(ttk.Frame):
                         "是" if config_value["mail"] else "否",
                         config_value["tag"] if config_value["tag"] else "None",
                         config_value["deadline"],
+                        config_value["note"],
                     )
 
             # 添加一行新的
@@ -298,6 +326,7 @@ class RoomLog(ttk.Frame):
                         "否",
                         "None",
                         "1970-01-01 00:00:00",
+                        "",
                     )
             add_button = ttk.Button(button_frame, width=52, bootstyle=DARK, text="添加", command=add)
             add_button.pack(fill=BOTH, side=LEFT)
@@ -311,6 +340,7 @@ class RoomLog(ttk.Frame):
                     mail = config_value_dict["mail"].get()
                     tag = config_value_dict["tag"].get()
                     deadline = config_value_dict["deadline"].get()
+                    note = config_value_dict["note"].get()
 
                     mail = True if mail == "是" else False
                     tag = None if tag == "None" else tag
@@ -319,6 +349,7 @@ class RoomLog(ttk.Frame):
                         "mail" : mail,
                         "tag" : tag,
                         "deadline" : deadline,
+                        "note" : note,
                     }
 
                 # 保存内存
@@ -328,11 +359,12 @@ class RoomLog(ttk.Frame):
 
                 self.func_insert_information("日志配置保存成功!当前配置:")
                 for name, config_dict in self.my_mail_type_config.items():
-                    self.func_insert_information("{0:<12} 邮件: {1:<6} 标签: {2:<10} 超时: {3}".format(
+                    self.func_insert_information("{0:<12} 备注: {4:<12} 邮件: {1:<6} 标签: {2:<10} 超时: {3}".format(
                         name,
                         "开" if config_dict["mail"] else "关",
                         "None" if config_dict["tag"] == None else config_dict["tag"],
                         config_dict["deadline"],
+                        config_dict["note"],
                     ))
 
             self.all_config_controls_dict = all_config_controls_dict
@@ -511,25 +543,153 @@ class RoomLog(ttk.Frame):
         scroll = ttk.Scrollbar(log_frmae)
         # side是滚动条放置的位置，上下左右。fill是将滚动条沿着y轴填充
         scroll.pack(side=RIGHT, fill=Y)
+
         # 配置几个颜色标签
-        # Crimson 深红/猩红
-        self.my_log_treeview.tag_configure('Crimson', background='Crimson')
-        self.my_tag_list.append('Crimson')
-        # Fuchsia 紫红/灯笼海棠
-        self.my_log_treeview.tag_configure('Fuchsia', background='Fuchsia')
-        self.my_tag_list.append('Fuchsia')
-        # DarkOrchid 暗兰花紫
-        self.my_log_treeview.tag_configure('DarkOrchid', background='DarkOrchid')
-        self.my_tag_list.append('DarkOrchid')
-        # LightSkyBlue 亮天蓝色
-        self.my_log_treeview.tag_configure('LightSkyBlue', background='LightSkyBlue')
-        self.my_tag_list.append('LightSkyBlue')
-        # MediumPurple 中紫色
-        self.my_log_treeview.tag_configure('MediumPurple', background='MediumPurple')
-        self.my_tag_list.append('MediumPurple')
-        # MediumPurple 中紫色
-        self.my_log_treeview.tag_configure('OrangeRed', background='OrangeRed')
-        self.my_tag_list.append('OrangeRed')
+        color_dict = {
+            'pink': '#FFC0CB 粉红',
+            'crimson': '#DC143C 深红/猩红',
+            'lavenderblush': '#FFF0F5 淡紫红',
+            'palevioletred': '#DB7093 弱紫罗兰红',
+            'hotpink': '#FF69B4 热情的粉红',
+            'deeppink': '#FF1493 深粉红',
+            'mediumvioletred': '#C71585 中紫罗兰红',
+            'orchid': '#DA70D6 暗紫色/兰花紫',
+            'thistle': '#D8BFD8 蓟色',
+            'plum': '#DDA0DD 洋李色/李子紫',
+            'violet': '#EE82EE 紫罗兰',
+            'magenta': '#FF00FF 洋红/玫瑰红',
+            'fuchsia': '#FF00FF 紫红/灯笼海棠',
+            'darkmagenta': '#8B008B 深洋红',
+            'purple': '#800080 紫色',
+            'mediumorchid': '#BA55D3 中兰花紫',
+            'darkviolet': '#9400D3 暗紫罗兰',
+            'darkorchid': '#9932CC 暗兰花紫',
+            'indigo': '#4B0082 靛青/紫兰色',
+            'blueviolet': '#8A2BE2 蓝紫罗兰',
+            'mediumpurple': '#9370DB 中紫色',
+            'mediumslateblue': '#7B68EE 中暗蓝色/中板岩蓝',
+            'slateblue': '#6A5ACD 石蓝色/板岩蓝',
+            'darkslateblue': '#483D8B 暗灰蓝色/暗板岩蓝',
+            'lavender': '#E6E6FA 淡紫色/熏衣草淡紫',
+            'ghostwhite': '#F8F8FF 幽灵白',
+            'blue': '#0000FF 纯蓝',
+            'mediumblue': '#0000CD 中蓝色',
+            'midnightblue': '#191970 午夜蓝',
+            'darkblue': '#00008B 暗蓝色',
+            'navy': '#000080 海军蓝',
+            'royalblue': '#4169E1 皇家蓝/宝蓝',
+            'cornflowerblue': '#6495ED 矢车菊蓝',
+            'lightsteelblue': '#B0C4DE 亮钢蓝',
+            'lightslategray': '#778899 亮蓝灰/亮石板灰',
+            'slategray': '#708090 灰石色/石板灰',
+            'dodgerblue': '#1E90FF 闪兰色/道奇蓝',
+            'aliceblue': '#F0F8FF 爱丽丝蓝',
+            'steelblue': '#4682B4 钢蓝/铁青',
+            'lightskyblue': '#87CEFA 亮天蓝色',
+            'skyblue': '#87CEEB 天蓝色',
+            'deepskyblue': '#00BFFF 深天蓝',
+            'lightblue': '#ADD8E6 亮蓝',
+            'powderblue': '#B0E0E6 粉蓝色/火药青',
+            'cadetblue': '#5F9EA0 军兰色/军服蓝',
+            'azure': '#F0FFFF 蔚蓝色',
+            'lightcyan': '#E0FFFF 淡青色',
+            'paleturquoise': '#AFEEEE 弱绿宝石',
+            'cyan': '#00FFFF 青色',
+            'aqua': '#00FFFF 浅绿色/水色',
+            'darkturquoise': '#00CED1 暗绿宝石',
+            'darkslategray': '#2F4F4F 暗瓦灰色/暗石板灰',
+            'darkcyan': '#008B8B 暗青色',
+            'teal': '#008080 水鸭色',
+            'mediumturquoise': '#48D1CC 中绿宝石',
+            'lightseagreen': '#20B2AA 浅海洋绿',
+            'turquoise': '#40E0D0 绿宝石',
+            'aquamarine': '#7FFFD4 宝石碧绿',
+            'mediumaquamarine': '#66CDAA 中宝石碧绿',
+            'mediumspringgreen': '#00FA9A 中春绿色',
+            'mintcream': '#F5FFFA 薄荷奶油',
+            'springgreen': '#00FF7F 春绿色',
+            'mediumseagreen': '#3CB371 中海洋绿',
+            'seagreen': '#2E8B57 海洋绿',
+            'honeydew': '#F0FFF0 蜜色/蜜瓜色',
+            'lightgreen': '#90EE90 淡绿色',
+            'palegreen': '#98FB98 弱绿色',
+            'darkseagreen': '#8FBC8F 暗海洋绿',
+            'limegreen': '#32CD32 闪光深绿',
+            'lime': '#00FF00 闪光绿',
+            'forestgreen': '#228B22 森林绿',
+            'green': '#008000 纯绿',
+            'darkgreen': '#006400 暗绿色',
+            'chartreuse': '#7FFF00 黄绿色/查特酒绿',
+            'lawngreen': '#7CFC00 草绿色/草坪绿',
+            'greenyellow': '#ADFF2F 绿黄色',
+            'darkolivegreen': '#556B2F 暗橄榄绿',
+            'yellowgreen': '#9ACD32 黄绿色',
+            'olivedrab': '#6B8E23 橄榄褐色',
+            'beige': '#F5F5DC 米色/灰棕色',
+            'lightgoldenrodyellow': '#FAFAD2 亮菊黄',
+            'ivory': '#FFFFF0 象牙色',
+            'lightyellow': '#FFFFE0 浅黄色',
+            'yellow': '#FFFF00 纯黄',
+            'olive': '#808000 橄榄',
+            'darkkhaki': '#BDB76B 暗黄褐色/深卡叽布',
+            'lemonchiffon': '#FFFACD 柠檬绸',
+            'palegoldenrod': '#EEE8AA 灰菊黄/苍麒麟色',
+            'khaki': '#F0E68C 黄褐色/卡叽布',
+            'gold': '#FFD700 金色',
+            'cornsilk': '#FFF8DC 玉米丝色',
+            'goldenrod': '#DAA520 金菊黄',
+            'darkgoldenrod': '#B8860B 暗金菊黄',
+            'floralwhite': '#FFFAF0 花的白色',
+            'oldlace': '#FDF5E6 老花色/旧蕾丝',
+            'wheat': '#F5DEB3 浅黄色/小麦色',
+            'moccasin': '#FFE4B5 鹿皮色/鹿皮靴',
+            'orange': '#FFA500 橙色',
+            'papayawhip': '#FFEFD5 番木色/番木瓜',
+            'blanchedalmond': '#FFEBCD 白杏色',
+            'navajowhite': '#FFDEAD 纳瓦白/土著白',
+            'antiquewhite': '#FAEBD7 古董白',
+            'tan': '#D2B48C 茶色',
+            'burlywood': '#DEB887 硬木色',
+            'bisque': '#FFE4C4 陶坯黄',
+            'darkorange': '#FF8C00 深橙色',
+            'linen': '#FAF0E6 亚麻布',
+            'peru': '#CD853F 秘鲁色',
+            'peachpuff': '#FFDAB9 桃肉色',
+            'sandybrown': '#F4A460 沙棕色',
+            'chocolate': '#D2691E 巧克力色',
+            'saddlebrown': '#8B4513 重褐色/马鞍棕色',
+            'seashell': '#FFF5EE 海贝壳',
+            'sienna': '#A0522D 黄土赭色',
+            'lightsalmon': '#FFA07A 浅鲑鱼肉色',
+            'coral': '#FF7F50 珊瑚',
+            'orangered': '#FF4500 橙红色',
+            'darksalmon': '#E9967A 深鲜肉/鲑鱼色',
+            'tomato': '#FF6347 番茄红',
+            'mistyrose': '#FFE4E1 浅玫瑰色/薄雾玫瑰',
+            'salmon': '#FA8072 鲜肉/鲑鱼色',
+            'snow': '#FFFAFA 雪白色',
+            'lightcoral': '#F08080 淡珊瑚色',
+            'rosybrown': '#BC8F8F 玫瑰棕色',
+            'indianred': '#CD5C5C 印度红',
+            'red': '#FF0000 纯红',
+            'brown': '#A52A2A 棕色',
+            'firebrick': '#B22222 火砖色/耐火砖',
+            'darkred': '#8B0000 深红色',
+            'maroon': '#800000 栗色',
+            'white': '#FFFFFF 纯白',
+            'whitesmoke': '#F5F5F5 白烟',
+            'gainsboro': '#DCDCDC 淡灰色',
+            'lightgrey': '#D3D3D3 浅灰色',
+            'silver': '#C0C0C0 银灰色',
+            'darkgray': '#A9A9A9 深灰色',
+            'gray': '#808080 灰色',
+            'dimgray': '#696969 暗淡灰',
+            'black': '#000000 纯黑',
+        }
+
+        for tag_name in color_dict:
+            self.my_log_treeview.tag_configure(tag_name, background=tag_name)
+            self.my_tag_list.append(tag_name)
 
         # 将文本框关联到滚动条上，滚动条滑动，文本框跟随滑动
         scroll.config(command=self.my_log_treeview.yview)
@@ -579,11 +739,11 @@ class RoomLog(ttk.Frame):
             self.func_insert_information("邮件功能暂停结束, 邮件功能恢复正常!")
 
         if self.my_mail_mode == 0:
-            # 切换到暂停 (最多暂停12小时)
+            # 切换到暂停 (最多暂停90s)
             self.my_mail_mode = 1
             self.mail_switch_btn.config(bootstyle=WARNING, image='mail-no')
-            # 12小时
-            sleep_time = 43200
+            # 90s
+            sleep_time = 90
             reset_date = datetime.now() + timedelta(seconds=sleep_time)
             self.func_insert_information("邮件功能暂停! {0} 恢复正常!".format(reset_date.strftime('%Y-%m-%d %H:%M:%S')))
             # 启动复原邮件的计时线程
@@ -639,7 +799,7 @@ class RoomLog(ttk.Frame):
 
     # =================== 用户函数 ===========================
     def func_update_user(self, user_name):
-        """ 初始化用户信息 """
+        """ 初始化或更新用户信息 """
         user_name = user_name
         user_ip = "10.88.3.152"
         user_sys_time = 'Time: {0}'.format(time.strftime('%Y-%m-%d %H:%M:%S'))
